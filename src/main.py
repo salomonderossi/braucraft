4c30404540ccf227aafedf5945a47f158108b254
salomonderossi <salomon@salomonderossi.de>
2013-10-22 17:58:42 +0000

    first layout

---
+++ src/main.py
@@ -0,0 +1,15 @@
+import kivy
+kivy.require('1.8.0')
+
+'''
+Check braucraft.kv for the ui design
+'''
+
+from kivy.app import App
+
+
+class BraucraftApp(App):
+    pass
+
+if __name__ == '__main__':
+    BraucraftApp().run()
