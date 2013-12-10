f95d9d4cd806dddd058bc6b644711045306f8a95
salomonderossi <salomon@salomonderossi.de>
2013-12-10 18:27:25 +0000

    Events from roulette scroller now working and more verbose names

--- src/main.py
+++ src/main.py
@@ -21,14 +21,18 @@ from kivy.logger import Logger as log
 from ds18b20 import DS18B20
 
 class TimerWidget(Screen):
-    
+
     def set_temperature(self, instance, temperature):
-        self.lbl_temperature.text = "%05.1f °C" % temperature
+        self.label_temperature.text = "%05.1f °C" % temperature
+
+    def on_time_changed(self, instance, time_value):
+        print instance
+        print time_value
 
 class MainWidget(Screen):
 
     def set_temperature(self, instance, temperature):
-        self.lbl_temperature.text = "%05.1f °C" % temperature
+        self.label_temperature.text = "%05.1f °C" % temperature
 
 
 class BraucraftApp(App, EventDispatcher):
@@ -38,8 +42,8 @@ class BraucraftApp(App, EventDispatcher):
 
     def build(self):
         log.info("Creating BrauCraft")
-        root = ScreenManager()
-        #root.transition = WipeTransition()
+        screen_manager = ScreenManager()
+        #screen_manager.transition = WipeTransition()
 
         log.info("Starting temperature reader thread")
         self.temp_sensor.start()
@@ -48,14 +52,15 @@ class BraucraftApp(App, EventDispatcher):
         log.info("Creating MainWidget")        
         main_widget = MainWidget(name="main")
         self.bind(temperature=main_widget.set_temperature)
-        root.add_widget(main_widget)
+        screen_manager.add_widget(main_widget)
 
         log.info("Creating TimerWidget")
         timer_widget = TimerWidget(name="timer")
         self.bind(temperature=timer_widget.set_temperature)
-        root.add_widget(timer_widget)
+        timer_widget.roulette_time.bind(rolling_value=timer_widget.on_time_changed)
+        screen_manager.add_widget(timer_widget)
 
-        return root
+        return screen_manager
 
     def read_temperature(self, *args):
         if not self.temp_sensor.temp_queue.empty():
